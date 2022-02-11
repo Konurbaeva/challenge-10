8970e981be9e91be70c4053d94e26ad41bd577fd
Konurbaeva <konurbaevamadina@gmail.com>
2022-02-11 22:20:31 +0100

    clean up

--- src/index.js
+++ src/index.js
@@ -48,51 +48,13 @@ function onSuccess(success) {
   console.log('response on success', success);
  }
 
-
   function onFetchError(error) {
     console.error(error);
    // Notiflix.Notify.failure('error: ', error);
    }
-
- /*  function renderCountry(countries) {
-   const countryMarkup = createCountryMarkup(countries);
-    countryInfo.insertAdjacentHTML('beforeend', countryMarkup);
-  }
- */
 
   function renderCountry(country) {
     const countryMarkup = countryTemplate(country);
      countryInfo.innerHTML = countryMarkup;
    }
    
- function createCountryMarkup(countries) {
-    console.log('countries: ', countries);
-
-    console.log('countries.name: ', countries[0].name);
-    console.log('countries.capital: ', countries[0].capital);
-    console.log('countries.population: ', countries[0].population);
-
-
-    const markup =  countries.map(({ name, capital, population, flags, languages } ) => {
-      return `
-      <div class="countries">
-      <img class="country-image" src="${flags.svg}" alt="${name}" />
-      <p class="country-capital">Capital: ${{capital}}</p>
-      <p class="country-population">Population: ${{population}}</p>
-      <div class="country-language"> Language: ${{languages}}</div>
-      </a></div>`
-      }).join("");
-       return markup;
-       
-
-/*     const markup =  countries.map(({ name, capital, population, flags, languages } ) => {
-        return `
-        <div class="countries">
-        <img class="country-image" src="${flags.svg}" alt="${name}" />
-        <p class="country-capital">Capital: ${{capital}}</p>
-        <p class="country-population">Population: ${{population}}</p>
-        <div class="country-language"> Language: ${{languages}}</div>
-        </a></div>`
-        }).join("");
-         return markup; */
-    }
