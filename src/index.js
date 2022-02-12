01bf80a1ec072ccd7ae26b5e2780c4e2fad85b36
Konurbaeva <konurbaevamadina@gmail.com>
2022-02-12 01:42:39 +0100

    added multiple countries template and if condition

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import debounce from 'lodash.debounce';
 // import getRefs from "./js/get-refs";
 import Notiflix from 'notiflix';
 import BASE_URL from "./js/api-service.js";
-import countryTemplate from './templates/countryHandleBar.hbs';
+import singleCountryTemplate from './templates/singleCountryTemplate.hbs';
+import multipleCountriesTemplate from './templates/multipleCountriesTemplate.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -12,19 +13,6 @@ const countryInfo = document.querySelector('.country-info');
 
 
 searchBox.addEventListener('input', debounce(filterCountriesChange, DEBOUNCE_DELAY));
-
-/* function fetchCountriesChange(e) {
-    BASE_URL.fetchCountries(searchBox.value)
-    .then(renderCountry)
-    .catch(onFetchError)
-  } */
-
-/* function filterCountriesChange() {
-  
-  BASE_URL.filterCountries(searchBox.value)
-    .then(renderCountry)
-    .catch(onFetchError)
-} */
 
 function filterCountriesChange() {
   
@@ -36,20 +24,25 @@ function filterCountriesChange() {
 
 function checkResponse(response) {
 
-  console.log('checkResponse: ' + response.message)
-
-  if(response.message === 'Not Found' || response.message === 'Page Not Found'){
-    Notiflix.Notify.failure("Oops, there is no country with that name");
-  } else {
-    renderCountry(response);
-  }
+    if(response.message === 'Not Found' || response.message === 'Page Not Found'){
+      Notiflix.Notify.failure("Oops, there is no country with that name");
+    } else if (response.length > 5) {
+      Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+    } else {
+      renderCountry(response);
+    }
 }
   function onFetchError(error) {
     console.error(error);
     Notiflix.Notify.failure('error: ', error);
    }
 
-  function renderCountry(country) {
-    const countryMarkup = countryTemplate(country).trim()
-     countryInfo.innerHTML = countryMarkup;
+  function renderCountry(response) {
+    if(response.length > 2 || response.length < 10) {
+      const countryMarkup = multipleCountriesTemplate(response).trim()
+      countryInfo.innerHTML = countryMarkup;
+    } else {
+      const countryMarkup = singleCountryTemplate(response).trim()
+      countryInfo.innerHTML = countryMarkup;
+    }
    }
